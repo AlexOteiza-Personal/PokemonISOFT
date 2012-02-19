3b59b3cbf66b86a26f5f4b9f5b1e5385bf21348b
Alejandro Garcia <agrs700@gmail.com>
2012-02-19 15:05:41 +0100

    Borrados archivos viejos

--- Pokemon/src/pokemon/imagedata/InvalidImageSizeException.java
+++
@@ -1,7 +0,0 @@
-package pokemon.imagedata;
-
-public class InvalidImageSizeException extends Exception {
-	public InvalidImageSizeException(int width, int height) {
-		super("Invalid image atributes: Width = "+ width + " Height = "+height);
-	}
-}
