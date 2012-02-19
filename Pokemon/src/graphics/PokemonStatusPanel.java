e3f8ff62fcba88d65b8f4d058b7d2663c93b9622
Alejandro Garcia <agrs700@gmail.com>
2012-02-19 19:28:54 +0100

    Modificada levemente la parte grafica

--- Pokemon/src/graphics/PokemonStatusPanel.java
+++ Pokemon/src/graphics/PokemonStatusPanel.java
@@ -4,18 +4,21 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 import pokemon.Pokemon;
+import pokemon.PokemonStats;
 import pokemon.imagedata.BattleImage;
 import pokemon.imagedata.ImageInvalidSizeException;
 
 
-public class PokemonStatusPanel extends JPanel {
-	private static Point pokemonOrigin = new Point(0, 0);
+public class PokemonStatusPanel extends JPanel{
+	private static Point pokemonOrigin = new Point(10, 0);
 	Pokemon pokemon;
 	public PokemonStatusPanel(Pokemon pokemon) throws IOException, ImageInvalidSizeException {
 		this.pokemon = pokemon;
@@ -24,10 +27,13 @@ public class PokemonStatusPanel extends JPanel {
 	public void paint(Graphics g) {
 		// TODO Auto-generated method 
 		g.drawImage (pokemon.getImageData().getImgFront().getImage(),pokemonOrigin.x,pokemonOrigin.y, null);
-		g.drawString(pokemon.getType1().toString(), 50, 120);
-		g.drawString(pokemon.getType2().toString(), 50, 140);
+		g.drawString(pokemon.getName(), 20, 100);
+		g.drawString("Tipo 1: "+pokemon.getType1().toString(), 20, 120);
+		g.drawString("Tipo 2: "+pokemon.getType2().toString(), 20, 140);
+		PokemonStats stats = pokemon.getStats();
+		g.drawString("Ataque: "+stats.getAttack(), 120, 100);
+		g.drawString("Defensa: "+stats.getDefense(), 120, 120);
+		g.drawString("Velocidad: "+stats.getSpeed(), 120, 140);
 	}
-
-	
 	
 }
